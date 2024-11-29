eae78609c57b788709184837963c5a68e0be65ad
chl218 <chl218@eng.ucsd.edu>
2024-11-29 02:38:40 -0800

    338. Counting Bits

---
+++ src/essential-75/bit-manip/338-counting-bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+    
+        res = []
+        for i in range(n+1):        
+            cnt = 0
+            while i > 0:    
+                cnt += i & 1
+                i = i >> 1
+                
+            res.append(cnt)
+            
