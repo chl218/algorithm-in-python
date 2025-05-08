30cf3274fc77f67ac94f624b4dfc40f96d7a5470
chl218 <chl218@eng.ucsd.edu>
2025-05-07 22:35:32 -0700

    1431. Kids With the Greatest Number of Candies

--- src/essential-75/01_array_strings/1071_greatest_common_divisor_of_strings.py
+++ src/essential-75/01_array_strings/1071_greatest_common_divisor_of_strings.py
@@ -1,11 +1,9 @@
 """ 1071. Greatest Common Divisor of Strings
 
-For two strings s and t, we say "t divides s" if and only if 
-s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more
-times).
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with
+itself one or more times).
 
-Given two strings str1 and str2, return the largest string x such that x
-divides both str1 and str2.
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
 
 
 Example 1:
@@ -23,12 +21,13 @@ Output: ""
 Constraints:
     1 <= str1.length, str2.length <= 1000
     str1 and str2 consist of English uppercase letters.
+
 """
 
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
 
-        
+
 # Brute Force
 # class Solution:
 #     def gcdOfStrings(self, str1: str, str2: str) -> str:
