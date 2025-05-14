bdeb9e779363cfd553ec79d3bca747d95a72e53f
chl218 <chl218@eng.ucsd.edu>
2025-05-13 23:46:51 -0700

    345. Reverse Vowels of a String

--- src/essential-75/01_array_strings/345_reverse_vowels_of_a_string.py
+++ src/essential-75/01_array_strings/345_reverse_vowels_of_a_string.py
@@ -2,32 +2,45 @@
 
 Given a string s, reverse only all the vowels in the string and return it.
 
-The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower
-and upper cases, more than once.
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
 
+ 
+Example 1:
+Input: s = "IceCreAm"
+Output: "AceCreIm"
+
+Explanation:
+The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+Example 2:
+Input: s = "leetcode"
+Output: "leotcede"
+
+
+Constraints:
+    1 <= s.length <= 3 * 105
+    s consist of printable ASCII characters.
 """
+
+
+# 2025/05/13
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        
-        
-        b, e = 0, len(s)-1
-        
-        
-        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-        
-        slst = [c for c in s]
-        
-        while b < e:
-            if s[b] in vowels and s[e] in vowels:
-                slst[b], slst[e] = slst[e], slst[b]
-                b += 1
-                e -= 1
-            elif s[b] in vowels:
-                e -= 1
-            elif s[e] in vowels:
-                b += 1
+        i, j = 0, len(s)-1
+        res = list(s)
+        vowels = "aeiouAEIOU"
+
+        while i < j:
+            if s[i] not in vowels:
+                i += 1
+            elif s[j] not in vowels:
+                j -= 1
+            elif s[i] in vowels and s[j] in vowels:
+                res[i], res[j] = res[j], res[i]
+                i += 1
+                j -= 1
             else:
-                b += 1
-                e -= 1
-                
-        return ''.join(slst)
+                return ''.join(res)
+        
+        return ''.join(res)
+            
