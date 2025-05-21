ce5f91783f7bf16d62762ce62a76e6ce3c6b4ab9
chl218 <chl218@eng.ucsd.edu>
2025-05-20 23:52:17 -0700

    283. Move Zeros

--- src/essential-75/01_array_strings/443_string_compression.py
+++ src/essential-75/01_array_strings/443_string_compression.py
@@ -37,24 +37,22 @@ Constraints:
 """
 
 class Solution:
-  def compress(self, chars: List[str]) -> int:
-    ans = 0
-    i = 0
-
-    while i < len(chars):
-      letter = chars[i]
-      count = 0
-      while i < len(chars) and chars[i] == letter:
-        count += 1
-        i += 1
-      chars[ans] = letter
-      ans += 1
-      if count > 1:
-        for c in str(count):
-          chars[ans] = c
-          ans += 1
-
-    return ans
+    def compress(self, chars: List[str]) -> int:
+        i = 0
+        res = 0
+        while i < len(chars):
+            group_length = 1
+            while (i + group_length < len(chars)
+                   and chars[i + group_length] == chars[i]):
+                group_length += 1
+            chars[res] = chars[i]
+            res += 1
+            if group_length > 1:
+                str_repr = str(group_length)
+                chars[res:res+len(str_repr)] = list(str_repr)
+                res += len(str_repr)
+            i += group_length
+        return res
   
 
 # 2025/05/19
--- src/essential-75/02_two_pointers/283_move_zeros.py
+++ src/essential-75/02_two_pointers/283_move_zeros.py
@@ -1,23 +1,42 @@
+""" 283. Move Zeros
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the
+non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+
+Constraints:
+    1 <= nums.length <= 104
+    -231 <= nums[i] <= 231 - 1
+
+Follow up: Could you minimize the total number of operations done?
+"""
+
+# 2025/05/20
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        i = j = 0
+        i, j = 0, 1
         n = len(nums)
-        while i < n and j < n:
+
+        while j < n:
             if nums[i] == 0:
-                while j < n and nums[j] == 0:
+                while nums[j] == 0:
                     j += 1
-                    
-                if j == n:
-                    return
-                
-                nums[i] = nums[j]
-                nums[j] = 0
-                
-                i += 1
-            else:
-                i += 1
-                j = i
+                    if j == n:
+                        return
+
+                nums[i], nums[j] = nums[j], 0
+            i += 1
+            j += 1
