12e9c0b4c19bf1c01eafa0fa2f3793c6d0ac6614
chl218 <chl218@eng.ucsd.edu>
2025-05-21 23:01:54 -0700

    392. Is Subsequence

--- src/essential-75/02_two_pointers/392_is_subsequence.py
+++ src/essential-75/02_two_pointers/392_is_subsequence.py
@@ -1,36 +1,46 @@
 """ 392. Is Subsequence
 
-Given two strings s and t, return true if s is a subsequence of t, or false
-otherwise.
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
-A subsequence of a string is a new string that is formed from the original
-string by deleting some (can be none) of the characters without disturbing the
-relative positions of the remaining characters. (i.e., "ace" is a subsequence
-of "abcde" while "aec" is no
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the
+characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of
+"abcde" while "aec" is not).
 
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+
+Constraints:
+    0 <= s.length <= 100
+    0 <= t.length <= 10^4
+    s and t consist only of lowercase English letters.
+
+Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 10^9, and you want to check one by one
+to see if t has its subsequence. In this scenario, how would you change your code?
 """
+
+# 2025/05/21
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        
-        tmap = {}
-        for idx, tt in enumerate(t):
-            if tt not in tmap:
-                tmap[tt] = [idx]
+
+        si, sj = 0, len(s)
+        ti, tj = 0, len(t)
+
+        matched = 0
+        while si < sj and ti < tj:
+
+            if s[si] == t[ti]:
+                si += 1
+                ti += 1
+                matched += 1
             else:
-                tmap[tt].append(idx)
-                
-        minIdx = -1
-        for ss in s:
-            if ss in tmap:
-                matched = False
-                for idx in tmap[ss]:
-                    if idx > minIdx:
-                        minIdx = idx
-                        matched = True
-                        break    
-                if not matched:
-                    return False
-            else:
-                return False
-            
-        return True
+                ti += 1
+
+        return True if matched == sj else False
