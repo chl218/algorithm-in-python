c82d1721d7a2ff6fdc89606d4b050dafcc21a349
chl218 <chl218@eng.ucsd.edu>
2025-06-05 23:58:57 -0700

    2215. Find the Difference of Two Arrays

--- src/essential-75/05_hash_map/2215_find_the-difference_of_two_arrays.py
+++ src/essential-75/05_hash_map/2215_find_the-difference_of_two_arrays.py
@@ -12,3 +12,36 @@ class Solution:
                 ans[1].append(i)
         
         return ans
+    
+
+
+    # class Solution:
+    # def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        
+    #     nums1Map = {}
+    #     nums2Map = {}
+
+    #     for n in nums1:
+    #         if n not in nums1Map:
+    #             nums1Map[n] = 1
+
+    #     for n in nums2:
+    #         if n not in nums2Map:
+    #             nums2Map[n] = 1
+
+
+        
+    #     nums1Diff = []
+    #     for n2 in nums2:
+    #         if n2 not in nums1Map:
+    #             nums1Diff.append(n2)
+    #             nums2Map[n2] = 1
+                
+    #     nums2Diff = []
+    #     for n1 in nums1:
+    #         if n1 not in nums2Map:
+    #             nums2Diff.append(n1)
+    #             nums2Map[n1] = 1
+
+
+    #     return [nums2Diff, nums1Diff]
