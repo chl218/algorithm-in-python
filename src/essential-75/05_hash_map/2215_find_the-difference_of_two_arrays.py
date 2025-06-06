2894578cf8c1ed7f52aabadd6e4704f935f1ab3a
chl218 <chl218@eng.ucsd.edu>
2025-06-06 00:05:13 -0700

    Comments

--- src/essential-75/05_hash_map/2215_find_the-difference_of_two_arrays.py
+++ src/essential-75/05_hash_map/2215_find_the-difference_of_two_arrays.py
@@ -1,3 +1,34 @@
+""" 2215. Find the Difference of Two Arrays
+
+Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+    answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+    answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+
+Note that the integers in the lists may be returned in any order.
+
+Example 1:
+
+Input: nums1 = [1,2,3], nums2 = [2,4,6]
+Output: [[1,3],[4,6]]
+Explanation:
+For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums1. Therefore, answer[1] = [4,6].
+
+
+Example 2:
+Input: nums1 = [1,2,3,3], nums2 = [1,1,2,2]
+Output: [[3],[]]
+Explanation:
+For nums1, nums1[2] and nums1[3] are not present in nums2. Since nums1[2] == nums1[3], their value is only included once and answer[0] = [3].
+Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
+
+Constraints:
+
+    1 <= nums1.length, nums2.length <= 1000
+    -1000 <= nums1[i], nums2[i] <= 1000
+"""
+
 class Solution:
     def findDifference(self, nums1, nums2):
         s1, s2 = set(nums1), set(nums2)
@@ -14,34 +45,25 @@ class Solution:
         return ans
     
 
-
-    # class Solution:
-    # def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        
-    #     nums1Map = {}
-    #     nums2Map = {}
-
-    #     for n in nums1:
-    #         if n not in nums1Map:
-    #             nums1Map[n] = 1
-
-    #     for n in nums2:
-    #         if n not in nums2Map:
-    #             nums2Map[n] = 1
-
-
-        
-    #     nums1Diff = []
-    #     for n2 in nums2:
-    #         if n2 not in nums1Map:
-    #             nums1Diff.append(n2)
-    #             nums2Map[n2] = 1
-                
-    #     nums2Diff = []
-    #     for n1 in nums1:
-    #         if n1 not in nums2Map:
-    #             nums2Diff.append(n1)
-    #             nums2Map[n1] = 1
-
-
-    #     return [nums2Diff, nums1Diff]
+# 2025/06/06
+# class Solution:
+#     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+#         nums1Map = {}
+#         nums2Map = {}
+#         for n in nums1:
+#             if n not in nums1Map:
+#                 nums1Map[n] = 1
+#         for n in nums2:
+#             if n not in nums2Map:
+#                 nums2Map[n] = 1
+#         nums1Diff = []
+#         for n2 in nums2:
+#             if n2 not in nums1Map:
+#                 nums1Diff.append(n2)
+#                 nums1Map[n2] = 1
+#         nums2Diff = []
+#         for n1 in nums1:
+#             if n1 not in nums2Map:
+#                 nums2Diff.append(n1)
+#                 nums2Map[n1] = 1
+#         return [nums2Diff, nums1Diff]
